c5de4001a1aa355c591e83e1669ccd4fe0fbadda
brunogentile <94723210+BrunoGentile@users.noreply.github.com>
2025-05-19 15:58:25 -0300

    Confeccionar Clase "GestionSucursales" 4.1 - Añadir conexión a Base de Datos. 4.2 - Añadir constructor vacío. 4.3 - Añadir constructor que tenga como parámetro un objeto de la clase Sucursal.

---
+++ TP7_GRUPO_15/Clases/Conexion.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TP7_GRUPO_15.Clases
+{
+    public class Conexion
+    {
+        private const string cadenaConexion = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=BDSucursales;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+
+        //CONSTRUCTOR
+        public Conexion() 
+        {
+        }
+
+
+
+        // OBTENGO CONEXIÓN CON LA BASE DE DATOS Y ABRO ESA CONEXIÓN ( ".Open" )
+        public SqlConnection ObtenerConexion()
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection(cadenaConexion);
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception exception)
+            {
+                return null;
+            }
+        }
+
+        // OBTENGO VARIABLE DONDE ALMACENO LA TABLA PEDIDA EN LA CONSULTA QUE RECIBE COMO PARÁMETRO
+        // ESTE MÉTODO TAMBIÉN ABRE LA CONEXIÓN YA QUE HACE LLAMADA AL METODO OBTENER CONEXIÓN
+        public SqlDataAdapter ObtenerAdaptador(string consultaSql)
+        {
+            SqlDataAdapter sqlDataAdapter;
+
+            try
+            {
+                sqlDataAdapter = new SqlDataAdapter(consultaSql, ObtenerConexion());
+                return sqlDataAdapter;
+            }
+            catch (Exception exception)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ TP7_GRUPO_15/Clases/GestionSucursales.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using TP7_GRUPO_15.Clases;
+
+namespace TP7_GRUPO_15.Clases
+{
+    public class GestionSucursales
+    {
+        private Conexion conexion;
+        private Sucursales sucursal;
+
+        // CONSTRUCTOR
+        public GestionSucursales()
+        {
+            conexion = new Conexion();
+        }
+
+        public GestionSucursales(Sucursales sucursal)
+        {
+            this.sucursal = sucursal;
+            conexion = new Conexion();
+        }
+
+      
+    }
+}
